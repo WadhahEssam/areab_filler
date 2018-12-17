32031ac179a2a4d0c540c04749e3905f4e44c575
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-12-17 10:36:35 +0300

    finishing the first save

--- areab_filler.js
+++ areab_filler.js
@@ -8,7 +8,7 @@ const puppeteer = require('puppeteer');
 
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.setViewport({height: 1080, width: 1920});
+  await page.setViewport({height: 800, width: 1200});
   await page.goto('localhost:8081/sign-in');
   
   ///////////////// Sign in ///////////////////////
@@ -26,9 +26,15 @@ const puppeteer = require('puppeteer');
 
   await page.goto(`http://localhost:8081/tasks/${taskID}/create`);
 
+
+  // testing  
+  await page.click('#react-select-12--value > div.Select-placeholder');
+  await page.keyboard.type('إرسال');
+  await page.keyboard.press(String.fromCharCode(13));
+
   //رقم التكليف 
   await page.click('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(1) > div > input');
-  await page.keyboard.type('123456');
+  await page.keyboard.type('32' + Math.floor(Math.random() * 999999) );
 
   // رمز العقار
   await page.click('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(2) > div > input')
@@ -69,10 +75,81 @@ const puppeteer = require('puppeteer');
   // جوال صاحب التقييم
   await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(5) > div:nth-child(2) > div > input','0556667474')
 
+  // اسم المالك
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(5) > div:nth-child(3) > div > input','صالح')
 
-  
-  
+  // هاتف اللعميل
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(5) > div:nth-child(4) > div > input','0556748333')
 
+  // منطقة ( Select Control )
+  await page.click('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__4.location.labelCol > div:nth-child(2) > div > div > div');
+  await page.keyboard.type('الرياض');
+  await page.keyboard.press(String.fromCharCode(13));
+  await page.waitFor(200);
+
+  // مدينة
+  await page.click('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__4.location.labelCol > div:nth-child(3) > div:nth-child(2) > div > div > div');
+  await page.keyboard.type('الرياض');
+  await page.keyboard.press(String.fromCharCode(13));
+  await page.waitFor(200);
+
+  // الحي
+  await page.click('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__4.location.labelCol > div:nth-child(4) > div:nth-child(2) > div > div > div');
+  await page.keyboard.type('الازدهار');
+  await page.keyboard.press(String.fromCharCode(13));
+
+  // شمال
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__6.boundaries.labelCol > div:nth-child(2) > div:nth-child(1) > div > input','30487')
+
+  // طول
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__6.boundaries.labelCol > div:nth-child(2) > div:nth-child(2) > div > input','6534')
+
+  // جنوب
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__6.boundaries.labelCol > div:nth-child(3) > div:nth-child(1) > div > input','1456')
+
+  // طول
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__6.boundaries.labelCol > div:nth-child(3) > div:nth-child(2) > div > input','1345')
+
+  // شرق
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__6.boundaries.labelCol > div:nth-child(4) > div:nth-child(1) > div > input','135456')
+
+  // طول
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__6.boundaries.labelCol > div:nth-child(4) > div:nth-child(2) > div > input','31543')
+
+  // غرب
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__6.boundaries.labelCol > div:nth-child(5) > div:nth-child(1) > div > input','1465')
+
+  // طول
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__4.flex.labelCol > div.flex__6.boundaries.labelCol > div:nth-child(5) > div:nth-child(2) > div > input','6875')
+
+  // Ceckboxes
+  await page.click('#inside_city');
+  await page.click('#water');
+  await page.click('#no_asphalt');
+
+  // خط طول و عرض
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.flex.flex_cont > div:nth-child(1) > div:nth-child(4) > div > div.labelRow > div:nth-child(1) > div > input','80')
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.flex.flex_cont > div:nth-child(1) > div:nth-child(4) > div > div.labelRow > div:nth-child(2) > div > input','120')
+
+  // اقرب شارب
+  await page.type('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.basic_info > div.flex > div.flex__5.labelCol > div:nth-child(3) > div:nth-child(1) > div > input','شارع الملك عبدالله')
+
+  // الجار
+  await page.click('#react-select-10--value > div.Select-placeholder');
+  await page.keyboard.type('مبني');
+  await page.keyboard.press(String.fromCharCode(13));
+
+  // المنسوب
+  await page.click('#react-select-9--value > div.Select-placeholder');
+  await page.keyboard.type('مرتفع');
+  await page.keyboard.press(String.fromCharCode(13));
+
+  // حفظ
+  await page.waitForSelector('.client-form > .row > .formButtons__cont > span > .labelMargin') 
+  await page.click('.client-form > .row > .formButtons__cont > span > .labelMargin') 
+  await page.waitForSelector('#body > .swal2-container > .swal2-popup > .swal2-actions > .swal2-confirm') 
+  await page.click('#body > .swal2-container > .swal2-popup > .swal2-actions > .swal2-confirm') 
+  
   await page.waitFor(2*1000);
   
   await browser.close();
